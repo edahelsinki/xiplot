5635bd1862dda6963fe061aa661936724823cf7b
Akihiro Tanaka <akihiro.fin@gmail.com>
2022-06-14 09:07:30 +0300

    Feature, user can choose command line app or dash app

--- index.py
+++ index.py
@@ -1,4 +1,10 @@
 from services.cmd_app import start
+import services.cmd_app as cmd_app
+import services.dash_app as dash_app
+from ui.cmd_renderer import get_user_command
 
 if __name__ == "__main__":
-    start()
+    if get_user_command == "1":
+        dash_app.start()
+    else:
+        cmd_app.start()
