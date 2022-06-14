f0cec8c6dd73fb1dccb87457f52a41039ff3d58a
Akihiro Tanaka <akihiro.fin@gmail.com>
2022-06-14 10:32:32 +0300

    Feature in the dash app, choose a data file from the data directory

--- services/dash_app.py
+++ services/dash_app.py
@@ -1,11 +1,28 @@
-from dash import Dash, html
+from sre_parse import State
+from dash import Dash, html, dcc, Output, Input, State
+import os
 
+
+data_files = [f for f in os.listdir(
+    "data") if os.path.isfile(os.path.join("data", f))]
 
 app = Dash(__name__)
 
 app.layout = html.Div(children=[
-    html.H1(children="Hello World!")
+    html.H3(children="Choose a data file"),
+    dcc.Dropdown(data_files, data_files[0], id="data_files", clearable=False),
+    html.Button("Load the data file", id="submit-button", n_clicks=0),
+    html.H3(id="chosen_data_file")
 ])
+
+
+@app.callback(
+    Output("chosen_data_file", "children"),
+    Input("submit-button", "n_clicks"),
+    State("data_files", "value")
+)
+def choose_data_file(n_clicks, filename):
+    return f"Data file: {filename}"
 
 
 def start():
