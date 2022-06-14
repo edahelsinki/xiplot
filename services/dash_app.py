264e8db8842023f4682ef69b5f29aa2dc92c017c
Akihiro Tanaka <akihiro.fin@gmail.com>
2022-06-14 09:29:12 +0300

    Initial dash app

---
+++ services/dash_app.py
@@ -0,0 +1,12 @@
+from distutils.log import debug
+from dash import Dash, html
+
+app = Dash(__name__)
+
+app.layout = html.Div(children=[
+    html.H1(children="Hello World!")
+])
+
+
+def start():
+    app.run_server()
