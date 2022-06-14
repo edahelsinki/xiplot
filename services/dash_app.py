052574fe706e4982eaf28d4aedf6ef49d1349e10
Akihiro Tanaka <akihiro.fin@gmail.com>
2022-06-14 10:57:01 +0300

    Added bootstrap for the dash app

--- services/dash_app.py
+++ services/dash_app.py
@@ -1,17 +1,21 @@
 from sre_parse import State
+
+from matplotlib.pyplot import cla
 from dash import Dash, html, dcc, Output, Input, State
+import dash_bootstrap_components as dbc
 import os
 
 
 data_files = [f for f in os.listdir(
     "data") if os.path.isfile(os.path.join("data", f))]
 
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div(children=[
     html.H3(children="Choose a data file"),
     dcc.Dropdown(data_files, data_files[0], id="data_files", clearable=False),
-    html.Button("Load the data file", id="submit-button", n_clicks=0),
+    html.Button("Load the data file", id="submit-button",
+                n_clicks=0, className="btn btn-primary"),
     html.H3(id="chosen_data_file")
 ])
 
