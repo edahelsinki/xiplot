9c65ef82db6c5c25e7796c8854954183968bb43a
Akihiro Tanaka <akihiro.fin@gmail.com>
2022-06-15 08:47:32 +0300

    Update docstring

--- ui/cmd_renderer.py
+++ ui/cmd_renderer.py
@@ -31,8 +31,8 @@ def show_scatter(df, xaxis, yaxis):
     """
         Renders a scatter plot.
 
-        Args
-        ----
+        Parameters
+        ----------
             df: Data frame
             xaxis: Unit(Column) of x axis
             yaxis: -"- y axis
@@ -46,8 +46,8 @@ def show_histogram(df, xaxis):
     """
         Renders a histogram.
 
-        Args
-        ----
+        Parameters
+        ----------
             df: Data frame
             xaxis: Unit(Columns) of x axis
     """
@@ -59,8 +59,8 @@ def show_selected_scatter(df, xaxis, yaxis, cdnts: Tuple):
     """
         Renders a subset of a scatter plot.
 
-        Args
-        ----
+        Parameters
+        ----------
             df: Data frame
             xaxis: Unit(Column) of x axis
             yaxis: -"- y axis
@@ -72,36 +72,85 @@ def show_selected_scatter(df, xaxis, yaxis, cdnts: Tuple):
 
 
 def show_start_screen():
+    """
+        Show a welcome message
+    """
     print("Welcome to the app!")
 
 
 def show_columns(df):
+    """
+        Show all columns of the given data
+
+        Parameters
+        ----------
+            df: Data frame
+    """
     print(f"""
         {df.columns.tolist()}
     """)
 
 
 def get_user_command():
+    """
+        Returns a command input from the user
+
+        Returns:
+            input(): User input as a string
+
+    """
     return input("Select a command: ")
 
 
 def get_app_selection():
+    """
+        Returns an input from the user, whether the user wants to use Dash or commandline version
+
+        Returns:
+            input(): User input as a string
+    """
     return input('Select the version to execute ("1": Dash, "2": Commandline): ')
 
 
 def get_user_file():
+    """
+        Returns the name of a data file
+
+        Returns:
+            input(): File name as a string
+
+    """
     return input("Pick a data file: ")
 
 
 def get_xaxis():
+    """
+        Returns an unit of the x axis of a histogram or a scatter
+
+        Returns:
+            input(): Unit as a string
+    """
     return input("Choose x axis: ")
 
 
 def get_yaxis():
+    """
+        Returns an unit of the y axis of a scatter
+
+        Returns:
+            input(): Unit as a string 
+    """
     return input("Choose y axis: ")
 
 
 def get_coordinates():
+    """
+        Returns a tuple of ranges of the x and y axis to create a subset of a scatter
+
+        Returns:
+            (x_min, x_max, y_min, y_max): Coordinates of the range of the x and y axis as a tuple
+            None: If invalid input
+    """
     try:
         x_min = int(input("Set smaller x value: "))
         x_max = int(input("Set greater x value: "))
@@ -114,18 +163,27 @@ def get_coordinates():
 
 
 def file_not_found(filename):
+    """
+        Shows an error message that the given file was not found in the directory "data".
+    """
     print(f"""
         File "{filename}" was not found in the directory "data"
     """)
 
 
 def file_not_given():
+    """
+        Shows an error message that any file has not been loaded yet.
+    """
     print("""
         Pick first a data file
     """)
 
 
 def invalid_column_name(xaxis, yaxis=None):
+    """
+        Shows an error message that the given name of x or y axis is not in the columns of the dataset.
+    """
     if not yaxis:
         print(f"""
         Invalid column name "{xaxis}"    
@@ -137,6 +195,9 @@ def invalid_column_name(xaxis, yaxis=None):
 
 
 def invalid_input(user_input=""):
+    """
+        Shows an error message that the given input was invaid
+    """
     print(f"""
         Invalid input {user_input}
     """)
