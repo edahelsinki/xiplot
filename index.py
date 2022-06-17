de02046f1917d752007b39ec469d4a0daa7fd0c8
Akihiro Tanaka <akihiro.fin@gmail.com>
2022-06-17 09:18:25 +0300

    Remove selection feature

--- index.py
+++ index.py
@@ -1,14 +1,16 @@
 from services.cmd_app import start
 import services.cmd_app as cmd_app
 import services.dash_app as dash_app
-from ui.cmd_renderer import get_app_selection
+#from ui.cmd_renderer import get_app_selection
 
 if __name__ == "__main__":
     """
         app_selection: User selects an app version. Either Dash app or commandline app.
     """
-    app_selection = get_app_selection()
+    dash_app.start()
+
+    """app_selection = get_app_selection()
     if app_selection == "1":
         dash_app.start()
     else:
-        cmd_app.start()
+        cmd_app.start()"""
