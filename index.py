cc742e981e3b7840de94f2b9b900d732ec545abf
Akihiro Tanaka <akihiro.fin@gmail.com>
2022-06-17 13:42:17 +0300

    Removed unused lines

--- index.py
+++ index.py
@@ -1,16 +1,6 @@
-from services.cmd_app import start
-import services.cmd_app as cmd_app
-import services.dash_app as dash_app
-#from ui.cmd_renderer import get_app_selection
+from services.dash import DashApp
 
 if __name__ == "__main__":
-    """
-        app_selection: User selects an app version. Either Dash app or commandline app.
-    """
-    dash_app.start()
 
-    """app_selection = get_app_selection()
-    if app_selection == "1":
-        dash_app.start()
-    else:
-        cmd_app.start()"""
+    app = DashApp()
+    app.start()
