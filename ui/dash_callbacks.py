b15669504f6e64760716497448b237eb9d22fe2f
Akihiro Tanaka <akihiro.fin@gmail.com>
2022-06-23 15:35:33 +0300

    Restructure callbacks, new feature: tabs

--- ui/dash_callbacks.py
+++ ui/dash_callbacks.py
@@ -1,6 +1,7 @@
 from dash import Output, Input, State, html
 from services.data_frame import read_data_file, get_kmean
 from services.graphs import *
+from services.dash_layouts import control_data_content, control_scatterplot_content, control_clusters_content
 from ui.dash_renderer import render_smiles
 import pandas as pd
 import numpy as np
@@ -13,50 +14,59 @@ class Callbacks:
 
     def get_callbacks(self, app):
         @app.callback(
-            Output("x_axis_histo", "options"),
-            Output("selected_data_column", "options"),
-            Output("scatter_target", "options"),
-            #Output("x_axis_histo", "value"),
-            Output("selected_data_column", "value"),
-            Output("scatter_target", "value"),
+            Output("control-tabs-content", "children"),
+            Input("control-tabs", "value"),
+            State("data_file_store", "data"),
+            State("scatterplot_store", "data"),
+        )
+        def control(tab, filename, user_inputs):
+            if tab == "control-data-tab":
+                return control_data_content(filename)
+            elif tab == "control-scatterplot-tab":
+                return control_scatterplot_content(user_inputs)
+            elif tab == "control-clusters-tab":
+                return control_clusters_content()
+
+        @app.callback(
+            Output("data_frame_store", "data"),
+            Output("data_file_store", "data"),
             Input("submit-button", "n_clicks"),
             State("data_files", "value"),
-            prevent_initial_call=True,
+            prevent_initial_call=True
         )
-        def choose_data_file(n_clicks, filename):
-            """
-                User chooses a data file to load. Column names are sent to show a histogram and a scatter. 
-
-                Returns:
-                    File name as a string and all the columns' names
-            """
+        def choose_file(n_clicks, filename):
             self.__df = read_data_file(filename)
-            columns = self.__df.columns.tolist()
-            return columns, columns, columns, columns[0], columns[0]
+            return self.__df.to_json(date_format="iso", orient="split"), filename
+
+        @app.callback(
+            Output("scatter_target", "options"),
+            Output("scatter_target_symbol", "options"),
+            Output("x_axis_histo", "options"),
+            Output("x_axis_histo", "value"),
+            Input("control-tabs", "value"),
+            State("data_frame_store", "data"),
+        )
+        def target(tab, data):
+            df = pd.read_json(data, orient="split")
+            columns = self.__df.columns.to_list()
+            return columns, columns, columns, columns[0]
 
         @app.callback(
             Output("scatterplot", "figure"),
+            Output("scatterplot-container", "style"),
+            Output("scatterplot_store", "data"),
+            Input("control-tabs", "value"),
             Input("algorythm", "value"),
             Input("scatter_target", "value"),
-            Input("scatter_cluster", "value"),
-            Input("jitter-button", "n_clicks"),
-            State("jitter-input", "value"),
-            prevent_initial_call=True
+            Input("scatter_target_symbol", "value"),
+            Input("jitter-input", "value"),
+            prevent_initial_call=True,
         )
-        def render_scatter(algorythm, color, n_clusters, n_clicks, jitter):
-            """
-                Returns a plotly's scatter object with axes given by the user.
-
-                Returns:
-                    Scatter object
-            """
-            x_axis = algorythm + " 1"
-            y_axis = algorythm + " 2"
-            if jitter is not None:
-                try:
-                    jitter = float(jitter)
-                except ValueError:
-                    raise "Invalid input"
+        def render_scatterplot(data, embedding, target, symbol, jitter):
+            x_axis = embedding + " 1"
+            y_axis = embedding + " 2"
+            if jitter:
+                jitter = float(jitter)
             df = copy.deepcopy(self.__df)
             if type(jitter) == float:
                 if jitter > 0:
@@ -64,94 +74,34 @@ class Callbacks:
                     Z = np.random.normal(Z, jitter)
                     jitter_df = pd.DataFrame(Z, columns=[x_axis, y_axis])
                     df[[x_axis, y_axis]] = jitter_df[[x_axis, y_axis]]
+            fig = Scatterplot(
+                df=df, x_axis=x_axis, y_axis=y_axis, color=target, symbol=symbol)
+            return fig.create_plot(), fig.style, fig.inputs
 
-            if n_clusters:
-                df = get_kmean(df, int(n_clusters), x_axis, y_axis)
-                fig = Scatterplot(df)
-                fig.set_symbol("Clusters")
-            else:
-                fig = Scatterplot(df)
-            fig.set_axes(x_axis, y_axis)
-            fig.set_color(color)
-            fig = fig.create_plot()
-            if n_clusters:
-                fig.update_layout(legend=dict(
-                    orientation="h",
-                    yanchor="bottom",
-                    y=1.02,
-                    xanchor="right",
-                    x=1
-                ))
-            return fig
-
-        @ app.callback(
+        @app.callback(
             Output("histogram", "figure"),
             Output("histo_mean", "children"),
             Output("histo_deviation", "children"),
+            Output("histogram-container", "style"),
             Input("x_axis_histo", "value"),
             Input("scatterplot", "selectedData"),
             prevent_initial_call=True,
         )
-        def render_histogram(x_axis, selected_data):
-            """
-                Returns a plotly's histogram object with a x axis given by the user
+        def render_histogram(x_axis, slct_data):
+            fig = Histogram(df=self.__df, x_axis=x_axis, barmode="overlay")
+            style = fig.style
 
-                Returns:
-                    Histogram object
-            """
-            fig = Histogram(self.__df)
-            fig.set_axes(x_axis)
+            points = [point["pointIndex"]
+                      for point in slct_data["points"]]
+            selected_df = self.__df.loc[self.__df.index.isin(points)]
+            color = px.colors.qualitative.Dark2
+            fig_2 = Histogram(selected_df, x_axis,
+                              color_dicrete_sequence=color)
+            fig_2 = fig_2.create_plot().data[0]
+            fig = fig.add_trace(fig.create_plot(), fig_2)
 
-            if selected_data:
-                points = [point["pointIndex"]
-                          for point in selected_data["points"]]
-                selected_df = self.__df.loc[self.__df.index.isin(points)]
-                fig_2 = Histogram(selected_df)
-                fig_2.set_axes(x_axis)
-                fig_2.set_color_discrete_sequence(px.colors.qualitative.Dark2)
-                fig_2 = fig_2.create_plot().data[0]
-                fig.set_barmode("overlay")
-                fig = fig.add_trace(fig.create_plot(), fig_2)
-
-                mean = round(self.__df[x_axis].mean(), 3)
-                deviation = round(self.__df[x_axis].std(), 3)
-                selected_mean = round(selected_df[x_axis].mean(), 3)
-                selected_deviation = round(selected_df[x_axis].std(), 3)
-                return fig, f"Full mean: {mean}, Selected mean: {selected_mean}", f"Full deviation: {deviation}, Selected deviation: {selected_deviation}"
-
-            fig = fig.create_plot()
             mean = round(self.__df[x_axis].mean(), 3)
             deviation = round(self.__df[x_axis].std(), 3)
-            return fig, f"Mean: {mean}", f"Deviation: {deviation}"
-
-        @app.callback(
-            Output("selected", "children"),
-            Input("scatterplot", "selectedData"),
-            prevent_initial_call=True
-        )
-        def selected_data(data):
-            points = [point["pointIndex"] for point in data["points"]]
-            return f"Selected {len(points)} points"
-
-        @app.callback(
-            Output("smiles_image", "children"),
-            Input("scatterplot", "hoverData"),
-            State("data_files", "value"),
-            prevent_initial_call=True
-        )
-        def render_mol_image(hover_data, filename):
-            df = read_data_file(filename)
-            point = hover_data["points"][0]["pointIndex"]
-            try:
-                smiles_str = df.loc[point]["SMILES"]
-                im = render_smiles(smiles_str)
-
-                children = [
-                    html.Img(
-                        src=im,
-                    ),
-                    html.P(smiles_str)
-                ]
-                return children
-            except KeyError:
-                return
+            selected_mean = round(selected_df[x_axis].mean(), 3)
+            selected_deviation = round(selected_df[x_axis].std(), 3)
+            return fig, f"Full mean: {mean}, Selected mean: {selected_mean}", f"Full deviation: {deviation}, Selected deviation: {selected_deviation}", style
