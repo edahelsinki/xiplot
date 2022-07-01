d42f55bac4da4025b7b4f393fe5578ef64da0211
Akihiro Tanaka <akihiro.fin@gmail.com>
2022-07-01 09:36:03 +0300

    Removed unused functions

--- services/data_frame.py
+++ services/data_frame.py
@@ -97,53 +97,6 @@ def read_auto_mpg_file(filename):
 def get_kmean(df, k: int, features):
     scaler = StandardScaler()
     scale = scaler.fit_transform(df[features])
-    # df_scale = pd.DataFrame(scale, columns=[x_axis, y_axis])
     km = KMeans(n_clusters=k).fit_predict(scale)
-    # y_predicted = km.fit_predict(df[[x_axis, y_axis]])
-    df["Clusters"] = km
-    df["Clusters"] = df["Clusters"].astype(str)
 
-    return df
-
-
-"""def modify_column_names(df: pd):
-    df.columns = pd.array(["mpg", "cylinders", "displacement", "horsepower", "weight", "acceleration",
-                           "model-year", "origin", "car-name"], dtype="U23")
-    return df
-
-
-def run_pca(df):
-    features = pd.array(["mpg", "cylinders", "displacement", "horsepower", "weight", "acceleration",
-                         "model-year", "origin"], dtype="U23")
-    x = df.loc[:, features].values
-    y = df.loc[:, ["car-name"]].values
-
-    x = StandardScaler().fit_transform(x)
-
-    # Fill NaN cells with the mean value of the column's values
-    mean_imputer = SimpleImputer(strategy="mean")
-    x = mean_imputer.fit_transform(x)
-
-    # Create a PCA object and set 2 dimensions
-    pca = PCA(n_components=2)
-    principalComponens = pca.fit_transform(X=x)
-
-    # Create a DataFrame object of the data that has been calculated with PCA
-    pal_df = pd.DataFrame(data=principalComponens, columns=[
-        "principal component 1", "principal component 2"])
-
-    # Concatenate
-    final_df = pd.concat(
-        [pal_df, df], axis=1)
-    return final_df
-
-
-def load_auto_mpg(filename):
-    widths = [7, 4, 10, 10, 11, 7, 4, 4, 30]
-    data = pd.read_fwf(f"data/{filename}", widths=widths,
-                       header=None, na_values=["?"])
-    df = pd.DataFrame(data)
-    df.columns = pd.array(["mpg", "cylinders", "displacement", "horsepower", "weight", "acceleration",
-                           "model-year", "origin", "car-name"], dtype="U23")
-    return df
-"""
+    return km
