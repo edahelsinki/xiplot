36c2c6b0b0d34f7b731652c8e7b5a283f17ebd5e
Akihiro Tanaka <akihiro.fin@gmail.com>
2022-07-21 11:33:47 +0300

    Made barplot interactive

--- dashapp/graphs/barplot.py
+++ dashapp/graphs/barplot.py
@@ -1,5 +1,3 @@
-import warnings
-
 import numpy as np
 import pandas as pd
 
@@ -8,9 +6,11 @@ import plotly.express as px
 from dash import html, dcc, Output, Input, State, MATCH
 
 from dashapp.utils.layouts import layout_wrapper, delete_button, cluster_dropdown
+from dashapp.utils.dataframe import get_numeric_columns
 from dashapp.graphs import Graph
 
 from collections import Counter
+from collections.abc import Iterable
 
 
 class Barplot(Graph):
@@ -18,26 +18,57 @@ class Barplot(Graph):
     def register_callbacks(app, df_from_store, df_to_store):
         @app.callback(
             Output({"type": "barplot", "index": MATCH}, "figure"),
+            Input({"type": "barplot_x_axis", "index": MATCH}, "value"),
+            Input({"type": "barplot_y_axis", "index": MATCH}, "value"),
             Input({"type": "bp_selection_cluster_dropdown", "index": MATCH}, "value"),
             Input({"type": "bp_comparison_cluster_dropdown", "index": MATCH}, "value"),
             Input("clusters_column_store", "data"),
             State("data_frame_store", "data"),
             prevent_initial_call=True,
         )
-        def tmp(selection, comparison, kmeans_col, df):
+        def tmp(x_axis, y_axis, selection, comparison, kmeans_col, df):
             return Barplot.render_barplot(
-                selection, comparison, kmeans_col, df_from_store(df)
+                x_axis, y_axis, selection, comparison, kmeans_col, df_from_store(df)
             )
 
     @staticmethod
-    def render_barplot(selection, comparison, kmeans_col, df):
+    def render_barplot(x_axis, y_axis, selection, comparison, kmeans_col, df):
         if len(kmeans_col) == df.shape[0]:
             df["Clusters"] = kmeans_col
-        fig = make_fig_fgs(df, selection, comparison, "reldiff", kmeans_col)
+        if y_axis == "frequency":
+            fig = make_fig_fgs(
+                df, x_axis, y_axis, selection, comparison, "reldiff", kmeans_col
+            )
+        else:
+            fig = px.bar(
+                df,
+                x_axis,
+                y_axis,
+                color="Clusters",
+                color_discrete_map={
+                    "all": px.colors.qualitative.Plotly[0],
+                    **{
+                        f"c{i+1}": c
+                        for i, c in enumerate(px.colors.qualitative.Plotly[1:])
+                    },
+                    "bg\u00A0": px.colors.qualitative.Plotly[0],
+                    **{
+                        f"c{i+1}\u00A0": c
+                        for i, c in enumerate(px.colors.qualitative.Plotly[1:])
+                    },
+                },
+            )
         return fig
 
     @staticmethod
     def create_new_layout(index, df, columns):
+        x = [
+            c
+            for c in columns
+            if isinstance(df[c][0], Iterable)
+            or type(df[c][0]) in (int, np.int32, np.int64)
+        ]
+        y = ["frequency"] + get_numeric_columns(df, columns)
         return html.Div(
             [
                 delete_button("plot-delete", index),
@@ -50,9 +81,9 @@ class Barplot(Graph):
                 layout_wrapper(
                     component=dcc.Dropdown(
                         id={"type": "barplot_x_axis", "index": index},
-                        value=columns[0],
+                        value=x[0],
                         clearable=False,
-                        options=columns,
+                        options=x,
                     ),
                     css_class="dd-double-left",
                     title="x axis",
@@ -60,9 +91,9 @@ class Barplot(Graph):
                 layout_wrapper(
                     component=dcc.Dropdown(
                         id={"type": "barplot_y_axis", "index": index},
-                        value=columns[1],
+                        value=y[0],
                         clearable=False,
-                        options=columns,
+                        options=y,
                     ),
                     css_class="dd-double-right",
                     title="y axis",
@@ -79,16 +110,23 @@ class Barplot(Graph):
         )
 
 
-def make_fig_fgs(df, cluster, comparison, order, clusters):
+def make_fig_fgs(df, x_axis, y_axis, cluster, comparison, order, clusters):
     fgs_a = Counter()
     fgs_b = Counter()
 
-    for c, fgs in zip(clusters, df["fgs"]):
-        if c == cluster or cluster == "all":
-            fgs_a.update(fgs)
+    if type(df[x_axis][0]) in (int, np.int32, np.int64, str):
+        for c, row in zip(clusters, df[x_axis]):
+            if c == cluster or cluster == "all":
+                fgs_a.update([row])
+            if c == comparison or comparison == "all":
+                fgs_b.update([row])
+    else:
+        for c, row in zip(clusters, df[x_axis]):
+            if c == cluster or cluster == "all":
+                fgs_a.update(row)
 
-        if c == comparison or comparison == "all":
-            fgs_b.update(fgs)
+            if c == comparison or comparison == "all":
+                fgs_b.update(row)
 
     comparison += "\u00A0"
 
@@ -123,22 +161,22 @@ def make_fig_fgs(df, cluster, comparison, order, clusters):
         {
             "Clusters": [cluster for _ in fgs_overall.most_common(10)]
             + [comparison for _ in fgs_overall.most_common(10)],
-            "fg": [fg for fg, _ in fgs_overall.most_common(10)]
+            x_axis: [fg for fg, _ in fgs_overall.most_common(10)]
             + [fg for fg, _ in fgs_overall.most_common(10)],
-            "frequency": [fgs_a[fg] for fg, _ in fgs_overall.most_common(10)]
+            y_axis: [fgs_a[fg] for fg, _ in fgs_overall.most_common(10)]
             + [fgs_b[fg] for fg, _ in fgs_overall.most_common(10)],
         }
     )
 
     fig_fgs = px.bar(
         fgs,
-        x="fg",
-        y="frequency",
+        x=x_axis,
+        y=y_axis,
         color="Clusters",
         barmode="group",
         hover_data={
             "Clusters": False,
-            "fg": False,
+            x_axis: False,
             "frequency": ":.2%",
         },
         color_discrete_map={
