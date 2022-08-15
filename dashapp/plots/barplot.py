Fixed: get_num_columns returns only columns, which all rows are numerical

dashapp/plots/barplot.py
@@ -144,7 +144,7 @@ class Barplot(Plot):
             c
             for c in columns
             if isinstance(df[c][0], Iterable)
-            or type(df[c][0]) in (int, np.int32, np.int64)
+            or df[c].dtype in (int, np.int32, np.int64)
         ]
         y_columns = ["frequency"] + get_numeric_columns(df, columns)
 

dashapp/utils/dataframe.py
@@ -223,7 +223,7 @@ def get_numeric_columns(df, columns):
     columns = [
         c
         for c in columns
-        if type(df[c][0]) in (int, float, np.int32, np.int64, np.float32, np.float64)
+        if df[c].dtype in (int, float, np.int32, np.int64, np.float32, np.float64)
     ]
     return columns
 