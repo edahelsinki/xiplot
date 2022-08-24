771a0e2d664a1f2c7f73bdbac33b1fb53f5f5176
Juniper Langenstein <juniper.langenstein@helsinki.fi>
2022-08-24 13:17:07 +0000

    Better default colour scheme for fg-bg clustering

--- xiplot/utils/cluster.py
+++ xiplot/utils/cluster.py
@@ -3,6 +3,6 @@ import plotly.express as px
 
 def cluster_colours():
     return {
-        "all": px.colors.qualitative.Plotly[0],
-        **{f"c{i+1}": c for i, c in enumerate(px.colors.qualitative.Plotly[1:])},
+        "all": px.colors.qualitative.Vivid[-1],
+        **{f"c{i+1}": c for i, c in enumerate(px.colors.qualitative.Vivid[:-1])},
     }
