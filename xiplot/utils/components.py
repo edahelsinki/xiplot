ba2dda5dc7cebb62367884d389305df0908f4f2c
Anton Björklund <anton.bjorklund@helsinki.fi>
2023-02-27 17:48:17 +0200

    documentation

--- xiplot/utils/components.py
+++ xiplot/utils/components.py
@@ -3,9 +3,19 @@ from dash import dcc, html
 
 
 class FlexRow(html.Div):
-    def __init__(self, *components, className: Optional[str] = None, **kwargs):
+    """
+    Create a Div with a "display: flex" style.
+    This makes the children appear on one line (with wrapping if necessary).
+    Any child with a `className="stretch"` will fill all the empty space of the line.
+    `dcc.Dropdown` automatically stretch with a minimum length (see `html_components.css`).
+
+    Example:
+        row = FlexRow(dcc.Dropown(["A", "B"]), html.Button("Accept"), id="example")
+    """
+
+    def __init__(self, *children, className: Optional[str] = None, **kwargs):
         if className is None:
             className = "flex-row"
         else:
             className = className + " flex-row"
-        super().__init__(children=list(components), className=className, **kwargs)
+        super().__init__(children=list(children), className=className, **kwargs)
