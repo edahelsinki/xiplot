dd33d6c2cd032ec6b6936e847e3ff43ea1cc45eb
Anton Björklund <anton.bjorklund@helsinki.fi>
2023-02-28 13:18:07 +0200

    function for generating dash id:s

---
+++ xiplot/utils/__init__.py
@@ -0,0 +1,33 @@
+from typing import Any, Dict, Optional, Union
+
+from dash import ALL, ALLSMALLER, MATCH
+
+
+def generate_id(
+    cls: Union[type, MATCH, ALL, ALLSMALLER],
+    index: Optional[Any],
+    subtype: Optional[str] = None,
+) -> Dict[str, Any]:
+    """Generate id:s for (dash) objects.
+
+    Since the type of the id is based on an actual type there is less chance for overloading and better refactoring resistance.
+    Furthermore, using this function instead of a handcrafted dict leads to fewer typos.
+
+    Args:
+        cls: The type for which the id is generated, e.g., a subclass of `xiplot.plots.Plot`.
+        index: The index of the id. Defaults to None.
+        subtype: If multiple id:s are generated for the same class (e.g. children of a `html.Div`) then this can be used for differentiation. Defaults to None.
+
+    Returns:
+        An id to be used with Dash.
+    """
+    if cls in [MATCH, ALL, ALLSMALLER]:
+        classtype = cls
+    elif subtype is None:
+        classtype = f"{cls.__module__}_{cls.__qualname__}".replace(".", "_")
+    else:
+        classtype = f"{cls.__module__}_{cls.__qualname__}_{subtype}".replace(".", "_")
+    if index is None:
+        return {"type": classtype}
+    else:
+        return {"type": classtype, "index": index}
