5248450f49af36de989560c0feb0f2075cc33011
Anton Björklund <anton.bjorklund@helsinki.fi>
2023-03-06 14:19:48 +0200

    Make settings persist across page reloads

--- xiplot/tabs/settings.py
+++ xiplot/tabs/settings.py
@@ -2,36 +2,47 @@ from xiplot.tabs import Tab
 from xiplot.utils.components import FlexRow
 from xiplot.utils.layouts import layout_wrapper
 
-from dash import html, Input, Output, dcc
+from dash import html, Input, Output, dcc, State, Dash
 
 
 class Settings(Tab):
-    def register_callbacks(app, df_from_store, df_to_store):
+    @staticmethod
+    def register_callbacks(app: Dash, df_from_store, df_to_store):
         app.clientside_callback(
             """
-            function toggleLightDarkMode(nClicks) {
-                if (nClicks % 2 == 1) {
-                    document.documentElement.setAttribute("data-theme", "dark")
-                    return 'Light mode'
+            function toggleLightDarkMode(clicks, data) {
+                if (clicks != undefined) {
+                    data = !data
                 }
-                document.documentElement.setAttribute("data-theme", "light")
-                return 'Dark mode'
+                if (data) {
+                    document.documentElement.setAttribute("data-theme", "dark")
+                    return ['Light mode', data]
+                } else {
+                    document.documentElement.setAttribute("data-theme", "light")
+                    return ['Dark mode', data]
+                }
             }
             """,
             Output("light-dark-toggle", "children"),
+            Output("light-dark-toggle-store", "data"),
             Input("light-dark-toggle", "n_clicks"),
+            State("light-dark-toggle-store", "data"),
+            prevent_initial_call=False,
         )
+
         app.clientside_callback(
             """
             function changeColSize(cols) {
                 document.documentElement.setAttribute("data-cols", cols)
-                return cols
+                return ' '
             }
             """,
-            Output("settings-column-size", "value"),
+            Output("settings-tab-dummy", "children"),
             Input("settings-column-size", "value"),
+            prevent_initial_call=False,
         )
 
+    @staticmethod
     def create_layout():
         return FlexRow(
             layout_wrapper(
@@ -42,15 +53,21 @@ class Settings(Tab):
                 ),
                 title="Colour scheme",
             ),
-            " ",
+            html.Span(" ", id="settings-tab-dummy"),
             layout_wrapper(
                 component=dcc.Dropdown(
                     ["1", "2", "3", "4", "5"],
                     "3",
                     clearable=False,
+                    persistence="True",
                     id="settings-column-size",
                 ),
                 title="Maximum number of columns",
             ),
             id="control-settings-container",
         )
+
+    @staticmethod
+    def create_layout_globals():
+        # Store the dark/light state across page reloads
+        return dcc.Store(id="light-dark-toggle-store", data=False, storage_type="local")
